5f50a63b2f52ff931086896f3ed6fbdb272e7b05
Ben Oliver <89800434+benolivr@users.noreply.github.com>
2024-11-14 12:55:37 -0700

    test push
    minor edit to test a push

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,11 +156,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-import environ 
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env(DEBUG=(bool, False))
 
 environ.Env.read_env(BASE_DIR / '.env')
 
